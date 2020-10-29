6f2100c8087cef7630f4114fd1a29adbf5efbe46
Administrator <65272329+Administrator859@users.noreply.github.com>
2020-10-29 23:28:49 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace APPLICATION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine();
+            Console.WriteLine();
+             Console.WriteLine();
+            Console.WriteLine("                                         $-**||JOB APPLICATION LETTER||**-$");
+              Console.WriteLine();
+            Console.WriteLine("           ****************************************************************************************************");
+               Console.WriteLine("                                          | Bendigosa, Cyrus Mydwyn Diaz |");
+            Console.WriteLine("           ====================================================================================================");
+                Console.WriteLine("           | Penafrncia Village, St. Cruz | 09263534278 | cymydbendigosa2004@gmail.com");
+            Console.WriteLine(); 
+             Console.WriteLine("                                                                                            City: Cotabato City");
+            Console.WriteLine();
+              Console.WriteLine("           Subject: Job Application letter \n           _______________________________"); 
+            Console.WriteLine();
+               Console.WriteLine("           Company: DMCI Holdings; Inc");
+            Console.WriteLine("           Date: 31 Jan 2016");
+                Console.WriteLine("           Daily Mail Manager");
+            Console.WriteLine("           State: Philippines");
+             Console.WriteLine("           Zip Code: 056");
+            Console.WriteLine();
+              Console.WriteLine();
+            Console.WriteLine("           Dear, Mr. John \n           ______________");
+               Console.WriteLine();
+            Console.WriteLine();
+                Console.WriteLine("              Here with I send an application letter and curriculum brand in response to your advertisement in");
+            Console.WriteLine("           the DMCI Holdings Inc, Saturday. 17 Jan 2016");
+             Console.WriteLine();
+            Console.WriteLine("              I have been experience over the past 9 years as manager of TV company and Paint fish production.");
+              Console.WriteLine("           Cyclorama Produaction. In the position I'm responsible for advertising in the davao erea. ");
+            Console.WriteLine("           Toril, Denver. Dallas");
+               Console.WriteLine();
+            Console.WriteLine("              I realize that my curriculum that i submit, this can't explain my qualifications in depth."); 
+                Console.WriteLine("           Therefore, I really hope there is a chance interview. Which I can share my thoughts or potential");
+            Console.WriteLine("           in me to help your company to get more customers outside the country.");  
+             Console.WriteLine();
+            Console.WriteLine();
+              Console.WriteLine("           Sincerely, \n           __________");
+            Console.WriteLine();
+               Console.WriteLine("           Bendigosa, Cyrus Mydwyn Diaz \n           ____________________________");
+            Console.WriteLine();
+                Console.WriteLine();
+            Console.WriteLine();
+        Console.ReadLine();
+        }
+    }
+}
